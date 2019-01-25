106742ab5c7c361131f47482d7fe881c59f60cfc
Mohamed El Sherif <mohamed.elsherif@100worte.de>
2019-01-25 17:58:46 +0100

    hack to fix

--- public/js/index.js
+++ public/js/index.js
@@ -110,11 +110,11 @@ function resizeNav() {
         return (getTotalWidthOfIds("theNav" + firstElement) + getTotalWidthOfIds("theNav" + ++firstElement)) / 2;
     }
 
-    function getNavArrayUntilId(x)
+    function getNavArrayFromUntilId(x,y)
     {
         var i=0;
         result=[];
-        for(var i=0;i<=x;i++)
+        for(var i=x;i<=y;i++)
         {
             var str = "theNav" + i;
             result.push(str);
@@ -124,12 +124,28 @@ function resizeNav() {
 
     function getLeftMargin(startingElementForByPass, x, y, elementsToBeChanged) 
     {
-        navElementsToByPass = getNumberOfNavElementsToByPassWhenComputingTheMargin(x, y)
-        var skippedPart = getTotalWidthOfIds(getNavArrayUntilId(navElementsToByPass - 1));
+        var  navElementsToByPass
+       if(y>x)
+       {
+           y=y-x;
+            navElementsToByPass=2
+            var skippedPart = getTotalWidthOfIds(getNavArrayFromUntilId(3,3));
+            var ff=skippedPart/2;
+            var widthOfElementWithNewMarginLeft = getTotalWidthOfIds("theNav" + elementsToBeChanged) / 2;
+            var sum = skippedPart;
+       return sum
+        
+       }else
+       {
+           navElementsToByPass = getNumberOfNavElementsToByPassWhenComputingTheMargin(x, y);  
+            var skippedPart = getTotalWidthOfIds(getNavArrayFromUntilId(startingElementForByPass,navElementsToByPass - 1));
         var betweenTwoNavs = getBetweenTwoNavsPosition(startingElementForByPass, navElementsToByPass);
         var widthOfElementWithNewMarginLeft = getTotalWidthOfIds("theNav" + elementsToBeChanged) / 2;
         var sum = skippedPart + betweenTwoNavs - widthOfElementWithNewMarginLeft;
         return sum
+       }
+       
+       
     }
 
     function putAllIdsInMiddle() 
@@ -141,6 +157,7 @@ function resizeNav() {
 
         dd = getMaxPossibleNavsInFirstRow()
         var magic=dd/numberOfElements*1.0>0.5?0:1;
+      
 //         if(magic==0)
 //         {
 //             var totalPossibleWidth = getWidthOfFirstNNavElements(dd);
@@ -160,7 +177,7 @@ function resizeNav() {
         console.log("magic"+ magic + "dd " + dd)
     }
 
-    var maxMarginAllowed = 47;//max margin
+    var maxMarginAllowed = 27;//max margin
     putAllIdsInMiddle();
     return;
 }
